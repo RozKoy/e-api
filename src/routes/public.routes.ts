e831331e4158b8e184824b0012536dba8982f6ff
Rangga Ndaru Anggoro <rangga.ndaru.a@fanintek.com>
2025-11-19 22:54:40 +0700

    feat: add get all and get one proposal in public routes

--- src/routes/public.routes.ts
+++ src/routes/public.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { AreaController } from '../controllers/area.controller';
 import { NewsController } from '@/controllers/news.controller';
+import { ProposalController } from '@/controllers/proposal.controller';
 
 export const publicRouter = Router();
 
 publicRouter.get('/areas/', AreaController.getAll);
 publicRouter.get('/news/', NewsController.getAll);
+publicRouter.get('/proposals', ProposalController.getAll);
 publicRouter.get('/news/:id', NewsController.getOneById);
+publicRouter.get('/proposals/:id', ProposalController.getOneById);
