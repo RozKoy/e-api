d776fa00f47a5a6917d694a9bebffae3a22421ee
Rangga Ndaru Anggoro <rangga.ndaru.a@fanintek.com>
2025-12-09 16:56:38 +0700

    feat: add routes for proposal discussions by proposalId and id

--- src/routes/public.routes.ts
+++ src/routes/public.routes.ts
@@ -5,6 +5,7 @@ import { ProposalController } from '@/controllers/proposal.controller';
 import { UserController } from '@/controllers/user.controller';
 import { CategoryController } from '@/controllers/category.controller';
 import { ProposalVoteController } from '@/controllers/proposalVote.controller';
+import { ProposalDiscussionController } from '@/controllers/proposalDiscussion.controller';
 
 export const publicRouter = Router();
 
@@ -12,8 +13,10 @@ publicRouter.get('/areas/', AreaController.getAll);
 publicRouter.get('/news/', NewsController.getAll);
 publicRouter.get('/categories', CategoryController.getAll);
 publicRouter.get('/proposals', ProposalController.getAll);
-publicRouter.get('/proposalVotes/count/:proposalId', ProposalVoteController.count);
 publicRouter.get('/proposals/year', ProposalController.getProposalYears);
+publicRouter.get('/proposalVotes/count/:proposalId', ProposalVoteController.count);
+publicRouter.get('/proposalDiscussions/proposal/:proposalId', ProposalDiscussionController.getByProposalId);
+publicRouter.get('/proposalDiscussions/:id', ProposalDiscussionController.getOneById);
 publicRouter.get('/users/', UserController.getAllPublic);
 publicRouter.get('/users/structural', UserController.getStructural);
 publicRouter.get('/news/:id', NewsController.getOneById);
